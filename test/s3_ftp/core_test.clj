cafefbbae9febe1eb3553b438fd745f8ef82ddb3
Wes Morgan <cap10morgan@gmail.com>
2014-05-20 11:58:27 -0600

    [WM JE] add a test for user-specific S3 bucket config
    Fails as of this commit, handing off to Jen to make it pass.

--- test/s3_ftp/core_test.clj
+++ test/s3_ftp/core_test.clj
@@ -1,7 +1,27 @@
 (ns s3-ftp.core-test
   (:require [clojure.test :refer :all]
-            [s3-ftp.core :refer :all]))
+            [s3-ftp.core :refer :all]
+            [aws.sdk.s3 :as s3]
+            [cemerick.bandalore :as sqs]
+            [miner.ftp :as ftp]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def s3-state (promise))
+
+(defn run-server [f]
+  (with-redefs [sqs/create-queue (constantly nil)
+                sqs/send (constantly nil)
+                s3/put-object (fn [_ bucket _ _]
+                                (deliver s3-state {:bucket bucket}))]
+    (let [server (start-server)]
+      (f)
+      (.stop server))))
+
+(use-fixtures :once run-server)
+
+(deftest user-config-test
+  (testing "files are uploaded to user-specific S3 bucket; not default"
+    (is (= "democracyworks-user1-ftp-test"
+           (do
+             (ftp/with-ftp [client "ftp://user1:admin@localhost:50021"]
+               (ftp/client-put client "README.md"))
+             (:bucket @s3-state))))))

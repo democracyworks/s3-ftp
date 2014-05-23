8d44e781dfc94d789be08c540bc913631e8bd521
Wes Morgan <cap10morgan@gmail.com>
2014-05-22 22:53:41 -0600

    [WM] add more tests user-specific config tests and refactor

--- test/s3_ftp/core_test.clj
+++ test/s3_ftp/core_test.clj
@@ -5,16 +5,17 @@
             [cemerick.bandalore :as sqs]
             [miner.ftp :as ftp]))
 
-(defn run-server [f]
-  (with-redefs [sqs/create-queue (constantly nil)
-                sqs/send (constantly nil)]
+(defn run-server [tests]
+  (with-redefs [sqs/create-queue (fn [_ queue-name] queue-name)
+                sqs/send (constantly nil)
+                s3/put-object (constantly nil)]
     (let [server (start-server)]
-      (f)
+      (tests)
       (.stop server))))
 
 (use-fixtures :once run-server)
 
-(deftest user-config-test
+(deftest s3-bucket-config-test
   (testing "files are uploaded to user-specific S3 bucket; not default"
     (let [s3-bucket (promise)]
       (with-redefs [s3/put-object (fn [_ bucket _ _]
@@ -23,7 +24,7 @@
                (do
                  (ftp/with-ftp [client "ftp://user1:admin@localhost:50021"]
                    (ftp/client-put client "README.md"))
-                 @s3-bucket))))))
+                 (deref s3-bucket 5000 nil)))))))
 
   (testing "files are uploaded to default S3 bucket when no user-specific bucket"
     (let [s3-bucket (promise)]
@@ -33,4 +34,23 @@
                (do
                  (ftp/with-ftp [client "ftp://user2:admin@localhost:50021"]
                    (ftp/client-put client "README.md"))
-                 @s3-bucket)))))))
+                 (deref s3-bucket 5000 nil))))))))
+
+(deftest sqs-queue-config-test
+  (testing "file upload notifications are sent to user-specific SQS queue; not default"
+    (let [sqs-queue (promise)]
+      (with-redefs [sqs/send (fn [_ queue _]
+                               (deliver sqs-queue queue))]
+        (is (= "democracyworks-user1-ftp-test"
+               (do (ftp/with-ftp [client "ftp://user1:admin@localhost:50021"]
+                     (ftp/client-put client "README.md"))
+                   (deref sqs-queue 5000 nil)))))))
+
+  (testing "file upload notifications are sent to default queue when no user-specific queue"
+    (let [sqs-queue (promise)]
+      (with-redefs [sqs/send (fn [_ queue _]
+                               (deliver sqs-queue queue))]
+        (is (= "democracyworks-s3-ftp-test"
+               (do (ftp/with-ftp [client "ftp://user2:admin@localhost:50021"]
+                     (ftp/client-put client "README.md"))
+                   (deref sqs-queue 5000 nil))))))))

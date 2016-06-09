8cfaacc6bccb549abfd75179836fbd9bf9b5bd91
Wes Morgan <wesmorgan@icloud.com>
2016-06-09 14:14:13 -0600

    Update some deps
    Didn't update resource-config b/c even after taking a pass through the code changes for 0.2.0 the tests were still failing and it's just not that important.

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "http://github.com/turbovote/s3-ftp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojure-tools "1.1.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clojure-tools "1.1.3"]
                  [org.apache.ftpserver/ftpserver-core "1.0.6" :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.ftpserver/ftplet-api "1.0.6"]
-                 [ch.qos.logback/logback-classic "1.0.13"]
-                 [clj-aws-s3 "0.3.8"]
-                 [com.cemerick/bandalore "0.0.5"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [clj-aws-s3 "0.3.10"]
+                 [com.cemerick/bandalore "0.0.6"]
                  [turbovote.resource-config "0.1.1"]]
   :resource-paths ["config", "resources"]
   :main s3-ftp.core
   :profiles {:build [:uberjar]
              :uberjar {:aot [s3-ftp.core]}
-             :test {:dependencies [[com.velisco/clj-ftp "0.3.0"]]
+             :test {:dependencies [[com.velisco/clj-ftp "0.3.6"]]
                     :resource-paths ["test-resources"]}}
   :uberjar-name "s3-ftp.jar")

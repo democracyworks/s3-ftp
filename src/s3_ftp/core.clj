02ea4bceb54737ccef27b11f94a0b44c33edd4c6
Troy Hill <troy@democracy.works>
2018-08-23 08:12:40 -0600

    move implicit ssl config

--- src/s3_ftp/core.clj
+++ src/s3_ftp/core.clj
@@ -93,14 +93,6 @@
                  (sqs/create-client))]
     (doto client (.setRegion (cfg/config [:aws :sqs :region])))))
 
-(defn- client-authentication
-  [ca-config]
-  (condp = ca-config
-    :need "need"
-    :want "want"
-    "none"))
-
-
 (defn- ssl-configuration [listener-factory config]
   "sets the ssl configuration on the ListenerFactory if the config exists"
   (if config
@@ -111,7 +103,6 @@
       (some->> config :keystore :password (.setKeystorePassword factory))
       (some->> ts-name io/resource io/file (.setTruststoreFile factory))
       (some->> config :truststore :password (.setTruststorePassword factory))
-      (some->> config :client-authentication client-authentication (.setClientAuthentication factory))
       (.setSslConfiguration listener-factory (.createSslConfiguration factory)))))
 
 (defn start-server []
@@ -120,6 +111,7 @@
         active-port (or (cfg/config [:ftp :active-port] 2221) 2221)
         listener-factory (doto (ListenerFactory.)
                            (.setPort active-port)
+                           (.setImplicitSsl (cfg/config [:ssl :implicit-ssl] false))
                            (.setDataConnectionConfiguration
                             (data-connection-configuration (cfg/config [:ftp])))
                            (ssl-configuration (cfg/config [:ssl] nil)))
